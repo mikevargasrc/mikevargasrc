acac0d2408f5b3e17bf929825c38527dafbad870
Maykol Vargas <43883413+mikevargasrc@users.noreply.github.com>
2023-06-20 10:31:29 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-	strings: ["Desarrollador Frontend", "Desarrollador Backend", "Desarrollador Full Stack"],
-	typeSpeed: 100,
-	backSpeed: 100,
-	backDelay: 1000,
-	loop: true
-})
