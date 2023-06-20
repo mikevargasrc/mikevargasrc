f824728b9e40bdfd00329136124805b4fa349dd3
Maykol Vargas <43883413+mikevargasrc@users.noreply.github.com>
2023-06-20 00:05:16 -0500

    v2.1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+	strings: ["Desarrollador Frontend", "Desarrollador Backend", "Desarrollador Full Stack"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+})
